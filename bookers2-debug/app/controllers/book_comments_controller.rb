5fa90c4e534729533cc625bb43ed80b2588acf8e
rui-21 <reisu6235@gmail.com>
2021-05-03 09:26:51 +0000

    [Add]応用課題3-エラーメッセージ追加

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -1,10 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      flash[:notice] = "You have created book comment successfully."
+      redirect_back(fallback_location: root_path)
+    else
+      @new_book = Book.new
+      render template: "books/show"
+    end
   end
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
